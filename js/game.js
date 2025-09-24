da46158be497c2c13472af26074dd2a6a7df2a18
CamdzicAlden <aldencamdzic3@gmail.com>
2025-09-24 22:14:59 +0200

    feat: Make cards flip on mouse click

--- js/game.js
+++ js/game.js
@@ -1,2 +1,8 @@
-const cards = document.getElementsByClassName(".card");
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    })
+})
 
