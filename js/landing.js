e477c9de9e972dc3a0c7aed9f66d2a2aa3ba72f3
CamdzicAlden <aldencamdzic3@gmail.com>
2025-09-24 18:26:00 +0200

    fix: Clean and comment everything

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,5 @@
-const playBtn = document.getElementById("playButton");
+const playBtn = document.getElementById("playButtonImg");
 
+//Play button hovering
 playBtn.onmouseover = () => playBtn.src = "./icons/PlayButtonHoverIcon.svg";
 playBtn.onmouseleave = () => playBtn.src = "./icons/PlayButtonIcon.svg";
